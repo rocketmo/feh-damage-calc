29069a2fc2aa19d04758f7397bea915b07e25841
winsomniak <winsomniak@gamedex.io>
2018-01-05 01:14:02 -0800

    Small tweak to data url for beta server

--- scripts/init/init.js
+++ scripts/init/init.js
@@ -53,7 +53,7 @@ var initFilters = true;
  * Pulling json data from kagerochart.
  */
 //$.getJSON('https://kagerochart.com/damage-calc/data', function(data) {
-$.getJSON('http://localhost:8080/damage-calc/data', function(data) {
+$.getJSON('/damage-calc/data', function(data) {
 	console.log('Retrieving hero data from KageroChart...');
 }).fail(function(res) {
 	console.log('Error retrieving data.');
